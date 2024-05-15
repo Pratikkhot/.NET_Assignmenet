af3edf8cdc41cf75bd4e7d980cdc5ff20a54cec7
Pratik_21 <khotpratik18@gmail.com>
2024-05-15 17:40:03 +0530

    .NET_Assignment

---
+++ Assignment_01/Q1.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_01
+{
+    internal class Q1
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Hello, World!");
+       // }
+    }
+}
---
+++ Assignment_01/Q2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_01
+{
+    internal class Q2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Four-Function Calculator!");
+
+            Console.Write("Enter the first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the operator (+, -, *, /): ");
+            char op = Convert.ToChar(Console.ReadLine());
+
+            double result;
+
+            switch (op)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine("Error: Division by zero");
+                        return;
+                    }
+                    result = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("Error: Invalid operator");
+                    return;
+            }
+
+            Console.WriteLine("Result: " + result);
+        }
+    }
+}
